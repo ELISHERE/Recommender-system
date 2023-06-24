9145d0f207ae07f028d1ff4ed8e88c439ce19107
Moeinhmz1679 <moein.hmz82@gmail.com>
2023-06-24 21:52:04 +0330

    getting user's input

--- Sports_field.py
+++ Sports_field.py
@@ -5,5 +5,9 @@ def sportRecom():
         df.fillna('Other', inplace=True)
         df.replace('Unknown', 'Other', inplace=True)
         return df
+    
     df = pd.read_csv('Sports_Field_Fixtures.csv')
     df = preprocess_data(df)
+    user_type = input("Enter the type of sport ground you are looking for: ")
+    user_material = input("Enter the material preference: ")
+    user_owner = input("Enter the owner preference: ")

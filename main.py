69b88a84ed9e928d0ed8e1fbd89145c955b5628e
mohamadmahdihabb <habibimohammadmahdi755@gmail.com>
2023-06-24 22:45:37 +0330

    hotel recommendation complete

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from Wedding_forum import weddingRecom
 from Commercial_realState import commercialRecom
 from Sports_field import sportRecom
+from hotel import hotelRecom
 while(True):
     print("1.Wedding forum recommendation system")
     print("2.Commercial real state recommendation system")
     print("3.Sport field recommendation system")
+    print("4.Hotel recommendation system")
     print("0.Exit")
     menu = int(input("Enter the menu number:"))
     if menu == 1:
@@ -13,5 +15,7 @@ while(True):
         commercialRecom()
     elif menu == 3:
         sportRecom()
+    elif menu == 4:
+        hotelRecom()
     elif menu == 0:
         break

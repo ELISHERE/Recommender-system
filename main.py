f7e84678a2b479c05faed292db40f5959ad9a5fa
Moeinhmz1679 <moein.hmz82@gmail.com>
2023-06-24 22:05:15 +0330

    adding sports field recommendation to main

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from Wedding_forum import weddingRecom
 from Commercial_realState import commercialRecom
+from Sports_field import sportRecom
 while(True):
     print("1.Wedding forum recommendation system")
     print("2.Commercial real state recommendation system")
+    print("3.Sport field recommendation system")
     print("0.Exit")
     menu = int(input("Enter the menu number:"))
     if menu == 1:
         weddingRecom()
     elif menu == 2:
         commercialRecom()
+    elif menu == 3:
+        sportRecom()
     elif menu == 0:
         break

e2498852321ed6a653b8206ad659832c4f962ec2
mhmdjheydari7 <mohammadjavadheydari1382@gmail.com>
2023-06-24 23:00:59 +0330

    with finding similarity based on feature of cars recommend nearest cars user wants

--- car.py
+++ car.py
@@ -5,4 +5,13 @@ def carsRecom():
     kilometers_driven = float(input("Enter kilometers driven: "))
     year = int(input("Enter year: "))
     car_condition = float(input("Enter car condition (between 0 and 5): "))
-    
+    data['Similarity'] = (
+        (data['Selling Price'] - selling_price) ** 2 +
+        (data['Kilometers Driven'] - kilometers_driven) ** 2 +
+        (data['Year'] - year) ** 2 +
+        (data['Car Condition'] - car_condition) ** 2
+    ) ** 0.5
+    data = data.sort_values('Similarity')
+    recommended_cars = data[['Model', 'Selling Price', 'Kilometers Driven', 'Year', 'Car Condition']].head(10)
+    print("Recommended Cars:")
+    print(recommended_cars)

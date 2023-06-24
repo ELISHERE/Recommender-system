72025c64c80f360152c391451bc78059d634a639
samanhasanlou <samanhasanlou19@gmail.com>
2023-06-24 21:28:53 +0330

    CommercialRecom function add to main.

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from Wedding_forum import weddingRecom
+from Commercial_realState import commercialRecom
 while(True):
     print("1.Wedding forum recommendation system")
+    print("2.Commercial real state recommendation system")
     print("0.Exit")
     menu = int(input("Enter the menu number:"))
     if menu == 1:
         weddingRecom()
+    elif menu == 2:
+        commercialRecom()
     elif menu == 0:
         break

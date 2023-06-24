0e7a3ae002dbb8b46ef396a83cb1cf15f896bfc7
mhmdjheydari7 <mohammadjavadheydari1382@gmail.com>
2023-06-24 23:03:11 +0330

    adding car's recommendation system to main

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from Wedding_forum import weddingRecom
 from Commercial_realState import commercialRecom
 from Sports_field import sportRecom
 from hotel import hotelRecom
+from car import carsRecom
 while(True):
     print("1.Wedding forum recommendation system")
     print("2.Commercial real state recommendation system")
     print("3.Sport field recommendation system")
     print("4.Hotel recommendation system")
+    print("5.Car recommendation system")
     print("0.Exit")
     menu = int(input("Enter the menu number:"))
     if menu == 1:
@@ -17,5 +19,7 @@ while(True):
         sportRecom()
     elif menu == 4:
         hotelRecom()
+    elif menu == 5:
+        carsRecom()
     elif menu == 0:
         break
